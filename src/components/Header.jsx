6b5dea6aa285cce87531f9a18ddffe1793c8d006
keinnybriceno <camilabrivel@gmail.com>
2023-06-17 15:22:56 -0300

    Cambios prueba

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,27 +2,27 @@ import ControlPresupuesto from "./ControlPresupuesto"
 import NuevoPresupuesto from "./NuevoPresupuesto"
 
 
-const Header = ({gastos, setGastos, presupuesto, setPresupuesto, isValuePresupuesto, setIsValuePresupuesto}) => {
+const Header = ({ gastos, setGastos, presupuesto, setPresupuesto, isValuePresupuesto, setIsValuePresupuesto }) => {
   return (
     <header>
-      <h1>Planificacion de Gastos</h1>
+      <h1>Planificación de Gastos</h1>
 
       {isValuePresupuesto ? (
-       <ControlPresupuesto
-       gastos={gastos}
-       setGastos={setGastos}
-       presupuesto={presupuesto}
-       setPresupuesto={setPresupuesto}
-       setIsValuePresupuesto={setIsValuePresupuesto}
-       />
-      ) :(
+        <ControlPresupuesto
+          gastos={gastos}
+          setGastos={setGastos}
+          presupuesto={presupuesto}
+          setPresupuesto={setPresupuesto}
+          setIsValuePresupuesto={setIsValuePresupuesto}
+        />
+      ) : (
         <NuevoPresupuesto
-        presupuesto={presupuesto}
-        setPresupuesto={setPresupuesto}
-        setIsValuePresupuesto={setIsValuePresupuesto}
+          presupuesto={presupuesto}
+          setPresupuesto={setPresupuesto}
+          setIsValuePresupuesto={setIsValuePresupuesto}
         />
       )}
-   
+
     </header>
   )
 }
